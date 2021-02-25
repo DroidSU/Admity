ea5ded281145a0e80e7c7db74e5c3a4d1e553817
kabir376 <70939526+kabir376@users.noreply.github.com>
2021-02-25 17:38:53 +0530

    Initial Commit

--- app/src/main/java/com/brixham/admity/MainActivity.kt
+++ app/src/main/java/com/brixham/admity/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.brixham.admity
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+            // This is used to hide the status bar and make
+            // the splash screen as a full screen activity.
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+                val backgroundimage: ImageView = findViewById(R.id.splashimage)
+                val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.animslide)
+                backgroundimage.startAnimation(slideAnimation)
+
+            // we used the postDelayed(Runnable, time) method
+            // to send a message with a delayed time.
+            Handler().postDelayed({
+                val intent = Intent(this, MainActivity2::class.java)
+                startActivity(intent)
+                finish()
+            }, 1500) // 3000 is the delayed time in milliseconds.
+        }
     }
-}
