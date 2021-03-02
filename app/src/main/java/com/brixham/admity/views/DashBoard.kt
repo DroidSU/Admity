89aff80095bc4c539090b8f4061c01a19aa47396
kabir376 <70939526+kabir376@users.noreply.github.com>
2021-03-02 18:59:50 +0530

    addedDashBoard - 02.03.2021

---
+++ app/src/main/java/com/brixham/admity/views/DashBoard.kt
@@ -0,0 +1,12 @@
+package com.brixham.admity.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.brixham.admity.R
+
+class DashBoard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash_board)
+    }
+}
--- app/src/main/java/com/brixham/admity/views/LoginScreen.kt
+++ app/src/main/java/com/brixham/admity/views/LoginScreen.kt
@@ -1,9 +1,18 @@
 package com.brixham.admity.views
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.button
+import android.content.Intent
+import android.graphics.Paint
 import android.os.Bundle
+import android.text.Html
+//import android.content.Intent
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import com.brixham.admity.R
 import com.brixham.admity.models.LoginResponseModel
@@ -18,6 +27,7 @@ import org.kodein.di.KodeinAware
 import org.kodein.di.android.closestKodein
 import org.kodein.di.generic.instance
 
+
 class LoginScreen : AppCompatActivity(), KodeinAware, NetworkCallback {
     override val kodein by closestKodein()
 
@@ -26,8 +36,19 @@ class LoginScreen : AppCompatActivity(), KodeinAware, NetworkCallback {
     private lateinit var btn_login : Button
     private lateinit var editTextUserId : TextInputEditText
     private lateinit var editTextPassword : TextInputEditText
+    private lateinit var alertDialog: AlertDialog
+    private lateinit var inflater: LayoutInflater
+    private lateinit var dialogview: View
+    private lateinit var builder: AlertDialog.Builder
+    private lateinit var textUnderline: TextView
 
-    var userId = ""
+
+
+    lateinit var msg: String
+
+
+
+    private var userId = ""
     var password = ""
     var TAG = LoginScreen::class.java.simpleName
 
@@ -37,12 +58,17 @@ class LoginScreen : AppCompatActivity(), KodeinAware, NetworkCallback {
         setContentView(R.layout.activity_login_screen)
 
         loginViewModel = ViewModelProviders.of(this, loginViewModelFactory).get(LoginViewModel::class.java)
-
+        //showProgress()
         btn_login = findViewById(R.id.btn_login)
         editTextUserId = findViewById(R.id.editText_sId)
         editTextPassword = findViewById(R.id.editText_password)
+        textUnderline = findViewById(R.id.textUnderline)
+        textUnderline.setText(Html.fromHtml(String.format(getString(R.string.your_string_here), )))
+        textUnderline.setPaintFlags(textUnderline.getPaintFlags() or Paint.UNDERLINE_TEXT_FLAG)
 
         btn_login.setOnClickListener {
+            val intent: Intent = Intent(this, DashBoard::class.java)
+            startActivity(intent)
             startLogin()
         }
     }
@@ -62,15 +88,31 @@ class LoginScreen : AppCompatActivity(), KodeinAware, NetworkCallback {
     }
 
     override fun callStarted() {
+
+
+
+
+
         Log.d(TAG, "callStarted: ")
     }
 
     override fun callFailed(errorMessage: String) {
-        Log.d(TAG, "callFailed: $errorMessage")
+
     }
 
     override fun callSuccess(data: Any) {
         val loginResponse = data as LoginResponseModel
         Log.d(TAG, "callSuccess: "+loginResponse.message)
     }
+
+    /*fun showProgress() {
+        val builder = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val dialogview = inflater.inflate(R.layout.dialog_loading, null)
+        alertDialog.setView(dialogview)
+        alertDialog = builder.create()
+        alertDialog.show()
+
+
+    }*/
 }
