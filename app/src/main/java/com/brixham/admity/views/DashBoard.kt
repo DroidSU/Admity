5e6a811f5cbcaa957cd3aa78b8356ca33d44b383
kabir376 <70939526+kabir376@users.noreply.github.com>
2021-03-04 13:51:24 +0530

    kabir_notification 04.03.2021

--- app/src/main/java/com/brixham/admity/views/DashBoard.kt
+++ app/src/main/java/com/brixham/admity/views/DashBoard.kt
@@ -1,12 +1,26 @@
 package com.brixham.admity.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import com.brixham.admity.R
 
 class DashBoard : AppCompatActivity() {
+    private lateinit var imgBellIcon: ImageView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash_board)
+        imgBellIcon = findViewById(R.id.imgBell)
+        imgBellIcon.setOnClickListener(View.OnClickListener {
+            var intent: Intent = Intent(this, Notification::class.java)
+            startActivity(intent)
+        })
+
+
     }
 }
