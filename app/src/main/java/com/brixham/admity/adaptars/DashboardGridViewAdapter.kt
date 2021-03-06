024bd2227637a97372ab70088264069c40f4df89
Sujoy Datta <sujoydatta26@gmail.com>
2021-03-06 19:07:19 +0530

    6/3/2021

---
+++ app/src/main/java/com/brixham/admity/adaptars/DashboardGridViewAdapter.kt
@@ -0,0 +1,28 @@
+package com.brixham.admity.adaptars
+//
+//import android.content.Context
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.BaseAdapter
+//
+//class DashboardGridViewAdapter(private val context : Context, private val layoutResourceId : Int) : BaseAdapter() {
+//    override fun getCount(): Int {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun getItem(position: Int): Any {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun getItemId(position: Int): Long {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+//        val itemView = convertView
+//
+//        if(itemView == null){
+//
+//        }
+//    }
+//}
--- app/src/main/java/com/brixham/admity/utilities/UtilityMethods.kt
+++ app/src/main/java/com/brixham/admity/utilities/UtilityMethods.kt
@@ -3,6 +3,7 @@ package com.brixham.admity.utilities
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import com.brixham.admity.R
 
@@ -10,7 +11,7 @@ import com.brixham.admity.R
 class UtilityMethods {
 
     fun showProgressDialog(context: Context) : AlertDialog{
-        val builder = AlertDialog.Builder(context)
+        val builder = AlertDialog.Builder(context, R.style.transparentDialog)
         val dialogView: View = LayoutInflater.from(context).inflate(R.layout.dialog_loading, null)
         builder.setView(dialogView)
 
@@ -20,8 +21,11 @@ class UtilityMethods {
     }
 
     fun showFailedDialog(context: Context, message : String) : AlertDialog{
-        val builder = AlertDialog.Builder(context)
+        val builder = AlertDialog.Builder(context, R.style.CustomAlertDialog)
         val dialogView: View = LayoutInflater.from(context).inflate(R.layout.dialog_failed, null)
+        val textView: TextView = dialogView.findViewById(R.id.tv_failed_dialog_body)
+        textView.text = message
+
         builder.setView(dialogView)
 
         return builder.create()
