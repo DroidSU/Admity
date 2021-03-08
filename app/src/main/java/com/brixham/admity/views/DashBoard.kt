4028429b51eb44c8d2e2ef19b960b2409402284a
Sujoy Datta <sujoydatta26@gmail.com>
2021-03-08 18:53:52 +0530

    Update DashBoard.kt

--- app/src/main/java/com/brixham/admity/views/DashBoard.kt
+++ app/src/main/java/com/brixham/admity/views/DashBoard.kt
@@ -1,26 +1,16 @@
 package com.brixham.admity.views
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import com.brixham.admity.R
-//import com.brixham.admity.adaptars.CustomGrid
 import com.google.android.material.navigation.NavigationView
 
-
 class DashBoard : AppCompatActivity() {
-
-
 
     private lateinit var imgBellIcon: ImageView
     private lateinit var dashboardImgCircleDP: ImageView
@@ -36,20 +26,21 @@ class DashBoard : AppCompatActivity() {
 
         linearLayoutMessage = findViewById(R.id.linearlayout_message)
         linearLayoutHelp = findViewById(R.id.help_linearlayout)
-        linearLayoutMessage.setOnClickListener(View.OnClickListener {
-            var intent: Intent = Intent(this, Messages::class.java)
+
+        linearLayoutMessage.setOnClickListener {
+            val intent = Intent(this, Messages::class.java)
             Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
-        })
-        linearLayoutHelp.setOnClickListener(View.OnClickListener {
-            var intent1: Intent = Intent(this, ChatAtivity::class.java)
+        }
+
+        linearLayoutHelp.setOnClickListener {
+            val intent1 = Intent(this, ChatAtivity::class.java)
             Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent1)
-        })
+        }
 
         dashBordDrawerLayout = findViewById(R.id.drawer_layout)
         dashboardImgCircleDP = findViewById(R.id.dashboardImgCircleDp)
-        //dashBordNavigationView = findViewById(R.id.nav_view)
 
         dashboardImgCircleDP.setOnClickListener {
             dashBordDrawerLayout.openDrawer(Gravity.RIGHT)
