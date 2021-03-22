7b2a57b8b5252271a68a2bf66bd3b026b9aa920d
Sujoy Datta <sujoydatta26@gmail.com>
2021-03-22 19:11:07 +0530

    Refactored code - sujoy 22/03/2021

--- app/src/main/java/com/brixham/admity/fragments/FragmentMessage.kt
+++ app/src/main/java/com/brixham/admity/fragments/FragmentMessage.kt
@@ -25,28 +25,13 @@ private const val ARG_PARAM2 = "param2"
  */
 class FragmentMessage : Fragment() {
 
-
-
     private lateinit var currentView: View
     private var layoutManager: RecyclerView.LayoutManager? = null
     private var adapter: RecyclerView.Adapter<MsgRecyclerAdapter.ViewHolder>? = null
     private lateinit var recycler_adaptar : RecyclerView
 
-
-
-
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-
-
-
-
-
-        }
     }
 
 
@@ -55,42 +40,20 @@ class FragmentMessage : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
-
-
-        showMessage()
-        //layoutManager = LinearLayoutManager(this)
-
-
+        currentView = inflater.inflate(R.layout.fragment_message, container, false)
 
         recycler_adaptar = currentView.findViewById(R.id.message_recycler_view)
         recycler_adaptar.layoutManager = layoutManager
         adapter = MsgRecyclerAdapter()
         recycler_adaptar.adapter = adapter
-    }
 
-    private fun showMessage() {
-        TODO("Not yet implemented")
+        return currentView
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentMessage.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
             FragmentMessage().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
             }
     }
 }
